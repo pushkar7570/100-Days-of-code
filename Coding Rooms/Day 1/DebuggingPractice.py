c3b6e3b0b969388627e5110608a81577b4566044
pushkar7570 <67285671+pushkar7570@users.noreply.github.com>
2022-08-01 13:03:41 +0530

    Create DebuggingPractice.py

---
+++ Coding Rooms/Day 1/DebuggingPractice.py
@@ -0,0 +1,9 @@
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#using ' ' in print function to print " " in the console
+
+#or using \n to print in next line so as to fit the code in single line
+# print('Day 1 - String Manipulation\nString Concatenation is done with the "+" sign.\ne.g. print("Hello " + "world")\nNew lines can be created with a backslash and n.')
