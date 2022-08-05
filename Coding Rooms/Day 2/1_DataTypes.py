7607434d54881aeb100dfcfcd532cda9fe6f189b
pushkar7570 <67285671+pushkar7570@users.noreply.github.com>
2022-08-05 12:54:31 +0530

    Create 1_DataTypes.py

---
+++ Coding Rooms/Day 2/1_DataTypes.py
@@ -0,0 +1,9 @@
+first_digit = int (two_digit_number)//10
+second_digit = int(two_digit_number)%10
+print(first_digit+second_digit)
+
+# or you can use below code
+
+# first = two_digit_number[1]
+# second = two_digit_number[0]
+# print(first + second)
