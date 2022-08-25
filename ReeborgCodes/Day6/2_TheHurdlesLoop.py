05f2fd5e11c46fc60e414742ee32e54ac8221447
pushkar7570 <67285671+pushkar7570@users.noreply.github.com>
2022-08-25 13:07:34 +0530

    Create 2_TheHurdlesLoop.py

---
+++ ReeborgCodes/Day6/2_TheHurdlesLoop.py
@@ -0,0 +1,21 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def move_oneBlock():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+move_oneBlock()
+move_oneBlock()
+move_oneBlock()
+move_oneBlock()
+move_oneBlock()
+move_oneBlock()
