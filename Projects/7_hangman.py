7cefd1f2179260190f459dcc522e2a5a53b591e5
buggy_bot <67285671+pushkar7570@users.noreply.github.com>
2022-08-28 00:45:39 +0530

    Create 7_hangman.py

---
+++ Projects/7_hangman.py
@@ -0,0 +1,97 @@
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+word_list = ["hangman", "mehul", "pushkar", "rajeev", "capture", "island", "shiv", "jai", "shree", "ram"]
+word = random.choice(word_list)
+word_len = len(word)
+
+blank_word = []
+for x in word:
+    blank_word.append("_")
+    
+print(blank_word)  
+
+lives = 6
+guessed_list = []
+
+while '_' in blank_word and lives >= 0:
+    bool = 1
+    letter = input("Guess the letter: ").lower()
+    guessed_list.append(letter)
+    
+    if guessed_list.count(letter) > 1:
+        print("You already guessed the letter. Try another!!")
+        continue
+        
+    for pos in range(word_len):
+        if letter == word[pos]:
+            bool = 0
+            blank_word[pos] = letter
+    
+    if bool == 1:
+        print(stages[lives])
+        lives -= 1
+        print(f"You are wrong and lost one life. Left : {lives+1}")
+    
+    print(blank_word)  
+    
+    if "_" not in blank_word:
+        print("You win!!")
+        break
+
+            
+            
